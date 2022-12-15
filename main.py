4cde16aa898a27834f4e1ebbbe5ca9a4e8c9799c
schaotix <rane29@gmail.com>
2022-12-15 15:18:07 -0500

    Working on collisions.

--- main.py
+++ main.py
@@ -10,11 +10,7 @@ screen = pygame.display.set_mode(size)
 pygame.display.set_caption("Space Bore")
 bg_img = pygame.image.load("space.jpg")
 
-# Screen setup variables
-first_col = width / 4 + random.randint(0, 300)
-second_col = width / 4 + random.randint(301, 600)
-third_col = width / 4 + random.randint(601, 900)
-fourth_col = width / 4 + random.randint(901, 1200)
+pygame.display.update()
 
 
 class Ship(object):
@@ -24,6 +20,7 @@ class Ship(object):
         self.ship_loc.center = width / 2, height * 0.8
         self.vel = 1
 
+
 class Rock(object):
     def __init__(self):
         self.rock = pygame.image.load("rock.png")
@@ -31,28 +28,27 @@ class Rock(object):
         self.rock_loc.center = -200, -200
         self.vel = 1
 
-pygame.display.update()
+
+def incomingRock():
+    #print(rock.rock_loc)
+    column = random.randint(0, 1200)
+    rock.rock_loc[1] += rock.vel
+    if rock.rock_loc[1] > height:
+        rock.rock_loc[1] = -200
+        rock.rock_loc.center = column, -200
+
 
 ship = Ship()
 rock = Rock()
 while running:
-    rock.rock_loc[1] += rock.vel
-    if rock.rock_loc[1] > height:
-        rock.rock_loc[1] = -200
-        coord = random.randint(0, 4)
-        if coord == 0:
-            rock.rock_loc.center = first_col, -200
-        if coord == 1:
-            rock.rock_loc.center = second_col, -200
-        if coord == 2:
-            rock.rock_loc.center = third_col, -200
-        if coord == 3:
-            rock.rock_loc.center = fourth_col, -200
+
+    incomingRock()
 
     for event in pygame.event.get():
         if event.type == QUIT:
             running = False
 
+    # Ship movement
     keys = pygame.key.get_pressed()
     if keys[K_RIGHT] and ship.ship_loc[0] <= width:
         ship.ship_loc[0] += ship.vel
@@ -63,9 +59,17 @@ while running:
         if ship.ship_loc[0] < 0:
             ship.ship_loc[0] = width
 
-    # Game Over
-    #if ship_loc[0] == rock_loc[1]+72:
-        #print("BOOM!")
+    if (rock.rock_loc[1] + rock.rock_loc[2] >= ship.ship_loc[1] and
+        rock.rock_loc[0] + rock.rock_loc[3] >= ship.ship_loc[0] and
+        rock.rock_loc[0] + rock.rock_loc[3] <= ship.ship_loc[0] + ship.ship_loc[3]):
+        collision = True
+        print("BOOM1")
+
+    if (rock.rock_loc[1] + rock.rock_loc[2] >= ship.ship_loc[1] and
+        rock.rock_loc[1] + rock.rock_loc[2] <= ship.ship_loc[0] and
+        rock.rock_loc[1] + rock.rock_loc[2] >= ship.ship_loc[0] + ship.ship_loc[3]):
+        collision = True
+        print("BOOM2")
 
     screen.blit(bg_img, (0, 0))
     screen.blit(ship.ship, ship.ship_loc)
