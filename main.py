57e64cb7e0aede351ca4e12b3d4ce174f586c20b
schaotix <screamer@hackermind.net>
2022-12-14 20:22:23 -0500

    Created classes.

--- main.py
+++ main.py
@@ -2,64 +2,74 @@ import pygame
 from pygame.locals import *
 import random
 
-size = width, height = (1200, 800)
-
+# Basic initializations
 pygame.init()
+size = width, height = (1200, 800)
 running = True
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("Space Bore")
-
-ship = pygame.image.load("ship.png")
-rock = pygame.image.load("rock.png")
 bg_img = pygame.image.load("space.jpg")
-vel = 1
 
-rock_loc = rock.get_rect()
+# Screen setup variables
 first_col = width / 4 + random.randint(0, 300)
 second_col = width / 4 + random.randint(301, 600)
 third_col = width / 4 + random.randint(601, 900)
 fourth_col = width / 4 + random.randint(901, 1200)
 
-ship_loc = ship.get_rect()
-ship_loc.center = width / 2, height * 0.8
+
+class Ship(object):
+    def __init__(self):
+        self.ship = pygame.image.load("ship.png")
+        self.ship_loc = self.ship.get_rect()
+        self.ship_loc.center = width / 2, height * 0.8
+        self.vel = 1
+
+class Rock(object):
+    def __init__(self):
+        self.rock = pygame.image.load("rock.png")
+        self.rock_loc = self.rock.get_rect()
+        self.rock_loc.center = -200, -200
+        self.vel = 1
 
 pygame.display.update()
 
+ship = Ship()
+rock = Rock()
 while running:
-    rock_loc[1] += 1
-    if rock_loc[1] > height:
-        rock_loc[1] = -200
+    rock.rock_loc[1] += rock.vel
+    if rock.rock_loc[1] > height:
+        rock.rock_loc[1] = -200
         coord = random.randint(0, 4)
         if coord == 0:
-            rock_loc.center = first_col, -200
+            rock.rock_loc.center = first_col, -200
         if coord == 1:
-            rock_loc.center = second_col, -200
+            rock.rock_loc.center = second_col, -200
         if coord == 2:
-            rock_loc.center = third_col, -200
+            rock.rock_loc.center = third_col, -200
         if coord == 3:
-            rock_loc.center = fourth_col, -200
+            rock.rock_loc.center = fourth_col, -200
 
     for event in pygame.event.get():
         if event.type == QUIT:
             running = False
 
     keys = pygame.key.get_pressed()
-    if keys[K_RIGHT] and ship_loc[0] <= width:
-        ship_loc[0] += vel
-        if ship_loc[0] > width:
-            ship_loc[0] = 0
-    if keys[K_LEFT] and ship_loc[0] >= 0:
-        ship_loc[0] -= vel
-        if ship_loc[0] < 0:
-            ship_loc[0] = width
+    if keys[K_RIGHT] and ship.ship_loc[0] <= width:
+        ship.ship_loc[0] += ship.vel
+        if ship.ship_loc[0] > width:
+            ship.ship_loc[0] = 0
+    if keys[K_LEFT] and ship.ship_loc[0] >= 0:
+        ship.ship_loc[0] -= ship.vel
+        if ship.ship_loc[0] < 0:
+            ship.ship_loc[0] = width
 
     # Game Over
-    if ship_loc[0] == rock_loc[1]+72:
-        print("BOOM!")
+    #if ship_loc[0] == rock_loc[1]+72:
+        #print("BOOM!")
 
     screen.blit(bg_img, (0, 0))
-    screen.blit(ship, ship_loc)
-    screen.blit(rock, rock_loc)
+    screen.blit(ship.ship, ship.ship_loc)
+    screen.blit(rock.rock, rock.rock_loc)
     pygame.display.update()
 
 pygame.quit()
